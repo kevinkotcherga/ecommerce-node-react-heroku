3b8edd33b29fc2bbd0d1bcf576b2549f4ea78055
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-11 14:09:42 +0200

    feat(index.js) : ajout de npm client

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use('/api/cart', cartRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
