0f3619fcfdeb419b434da5b576468ad3fa79f8c6
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-11 14:56:50 +0200

    feat(react) : modification des liens localhost vers heroku

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://ecommerce-react-node-kotcherga.herokuapp.com/api/';
 const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMjg3MDQyNWJjZThkMzVmMGM2OGUwZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NjkwNjE3NSwiZXhwIjoxNjQ3MTY1Mzc1fQ.4FMLXZwjvZ_sqpgiedl5W4IUq8_p5Q-NEHlRFpPANRI";
 
 
